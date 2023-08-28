419f6325f95b7ba2a48dad7ef47a2899740f1364
Brandy Bode <bbode6@huskers.unl.edu>
2023-08-28 10:26:44 -0500

    I added my name and major.

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Sophia Harding");
 		System.out.println("Computer Science");
-		}
-	
+		System.out.println("Brandy Bode");
+		System.out.println("Computer Science and Finance");
+	}
+
 }
